96e45b3077e21da6569daa85d77ee7ee769a11db
AlanVT <alanvitas27@gmail.com>
2022-09-01 16:24:59 -0300

    Cambios2

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+//----Buscador de multiplos, divisores y numeros primos---
+
 // let limite = parseInt(prompt("ingresa un numero"))
+
 // var arr = []
 // for (let i = 0; i < limite; i++) {
 //     if(parseInt(limite/(i+1))===limite/(i+1)){
@@ -22,19 +25,17 @@
 //     fct++ 
 // }
 
+//__________________________________________________________________________
 
+// Simulador
 
-
-
-
-// Definicion       es
+// Definiciones
 let total = 0
 let productosAgregados = []
-// productos = [
-//     "art1" = new articulo("Articulo1", 100),
-//     "art2" = new articulo("Articulo2", 1000),
-// ]
-const day =  new Date().getUTCDate()+"-"+new Date().getMonth()+"-"+ new Date().getUTCFullYear()
+let month = parseInt(new Date().getMonth()) + 1
+let day = new Date().getUTCDate()
+let year = new Date().getUTCFullYear()
+const date = month+"-"+day+"-"+ year
 const nombrePersona = prompt("Como te llamas?")
 class articulo{
     constructor(nombre ,precio){
@@ -49,7 +50,7 @@ class compras{
     this.fecha = fecha
     }
   }
-// ---------
+// _______________________________
 
 // Funciones
 function sumarValor(valor, producto){
@@ -58,52 +59,50 @@ function sumarValor(valor, producto){
 }
 
 function carro(){
+    if(total !=0){
     if(total >= 10000){
         let totalcd = total - (total*0.10)
-        if(total !=0){
         console.log("En total deberia pagar:", totalcd,"el descuento es ", total*0.10 , " y va a llevar:", productosAgregados)
         let comprar = confirm("Lo quiere comprar?")
         if(comprar == true){
-                totalcd = 0
-                productosAgregados = []
-                alert("Comprando...")
-                alert("espere un segundo...")
-                alert("Comprado!")
-                // new compras(nombrePersona, total, new Date().getDate())
-                
-                let recipt = confirm("quiere recivo?")
-                if (recipt == true) {
-                    const compra = new compras(nombrePersona, total, day)
-                    console.log('info compra ',compra)
-                }
+            totalcd = 0
+            productosAgregados = []
+            alert("Comprando...")
+            alert("espere un segundo...")
+            alert("Comprado!")
+            // new compras(nombrePersona, total, new Date().getDate())
+            let recipt = confirm("quiere recivo?")
+            if (recipt == true) {
+                const compra = new compras(nombrePersona, total, date)
+                console.log('info compra ',compra)
             }
             total = 0
         }
-        else{
-            alert("Usted no compro nada, toque algun articulo para añadirlo al")
-        }
+        total = 0
+
     }
     else{
-        if(total !=0){
         console.log("En total deberia pagar:", total, " y va a llevar:", productosAgregados)
         let comprar = confirm("Lo quiere comprar?")
         if(comprar == true){
             productosAgregados = []
-                alert("Comprando...")
-                alert("espere un segundo...")
-                alert("Comprado!")
-                let recipt = confirm("quiere recivo?")
-                if (recipt == true) {
-                    const compra = new compras(nombrePersona, total, day)
-                    console.log('info compra ',compra)
-                }
-        }
+            alert("Comprando...")
+            alert("espere un segundo...")
+            alert("Comprado!")
+            let recipt = confirm("quiere recivo?")
+            if (recipt == true) {
+                const compra = new compras(nombrePersona, total, date)
+                console.log('info compra ',compra)
+            }
             total = 0
         }
-        else{
-            alert("Usted no compro nada, toque algun articulo para añadirlo al carro")
-        }
+        total = 0
+    } 
     }
+    else{
+        alert("Usted no compro nada, seleccione algun producto.")
+    }
+
 }
 
 // ---------
