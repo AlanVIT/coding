321f30f2cd779c41f56d0038edc4c90944eae6aa
AlanVT <alanvitas27@gmail.com>
2022-09-29 16:33:22 -0300

    Librerias

--- carro.js
+++ carro.js
@@ -15,11 +15,12 @@ let tablaLs = document.getElementById("tabla")
 let comprar = document.getElementById("comprar")
 comprar.addEventListener("click", carro)
 
+let h5Sacar = document.querySelector(`#llevar`)
 let recibos = document.querySelector("#recibos")
 
 //Funciones
 function ponerProductos(){
-    let filas
+    let ponerPrecio
     productosCarrito.sort()
     productosCarrito.forEach(producto =>{        
         let prod = `<div class="card" style="width: 180px;height: 95px;">
@@ -30,8 +31,8 @@ function ponerProductos(){
     <br>`
     prd.innerHTML += prod
     })
-    filas =`<h5>En total va a gastar ${precio}$</h5>`
-    tablaLs.innerHTML += filas
+    ponerPrecio =`<h5>En total va a gastar ${precio}$</h5>`
+    tablaLs.innerHTML += ponerPrecio
 
     // for (i = 0; i < productosCarrito.length; i++) {
     //     // debugger
@@ -60,59 +61,106 @@ function ponerProductos(){
 
 ponerProductos()
 
+const reinicio = () =>{
+    totalcd = 0
+    productosCarrito = []
+    precio = 0
+
+    localStorage.setItem("Productos", JSON.stringify(productosCarrito))
+    localStorage.setItem("Precio", JSON.stringify(precio))
+    
+    h5Sacar.innerHTML = "Ya compro lo seleccionado, por lo que no va a llevar nada, por lo que no va a gastar nada"
+    prd.innerHTML = ""
+    tablaLs.innerHTML = ""
+}
 function carro(){
     if(precio !=0 ){
         if(precio >= 50000){
-            var totalcd = precio - (precio*0.10)
+            var totalcd = precio*0.9
         }
         else{
             var totalcd = precio
         }
-        if(confirm("Lo quiere comprar?")){
-            alert("Comprando...")
-            alert("Comprado!")
-            if (confirm("quiere recibo?")) {
-                let compra = new compras(nombrePersona, totalcd, date)
-                let recibo = `
-                <br>
-                <div class="card" style="width: 180px;height: 310px;">
-                <div class="card-body">
-                    <h5 class="card-title">${"Su recibo:"}</h5>
-                    <p class="card-text">${"Emitido por: "+compra.nombre}</p>
-                    <p>_____________________</p>
-                    <p class="card-text">${"TOTAL: "+compra.precio+"$"}</p>
-                    <p>_____________________</p>
-                    <p class="card-text">${"Emitido el: "+compra.fecha}</p>
-                </div>
-            </div>`
-                recibos.innerHTML += recibo
-
+        Swal.fire({
+            title: 'Lo quiere comprar?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Comprado'
+                )
+                Swal.fire({
+                    title: 'Quiere recibo?',
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Si!'
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                        let compra = new compras(nombrePersona, totalcd, date)
+                        let recibo = `
+                        <br>
+                        <div class="card" style="width: 180px;height: 310px;">
+                        <div class="card-body">
+                            <h5 class="card-title">${"Su recibo:"}</h5>
+                            <p class="card-text">${"Emitido por: "+compra.nombre}</p>
+                            <p>_____________________</p>
+                            <p class="card-text">${"TOTAL: "+compra.precio+"$"}</p>
+                            <p>_____________________</p>
+                            <p class="card-text">${"Emitido el: "+compra.fecha}</p>
+                        </div>
+                        </div>`
+                        recibos.innerHTML += recibo  
+                        reinicio()
+                    }
+                    else{
+                        reinicio()
+                        Swal.fire({
+                            position: 'top-end',
+                            timer: 2000,
+                            title:"Ok, no le damos recibo"
+                        })
+                    }
+                  })
             }
-            else{
-                alert("Ok, no le daremos recibo")
-            }
-            totalcd = 0
-            productosCarrito = []
-            precio = 0
-
-            localStorage.setItem("Productos", JSON.stringify(productosCarrito))
-            localStorage.setItem("Precio", JSON.stringify(precio))
-        }
         else{
             let numRandom = parseInt(Math.random()*3)
             if(numRandom == 0){
-            console.log("Ok, tomese su tiempo " + nombrePersona)
+                Swal.fire({
+                    position: 'top-end',
+                    timer: 2000,
+                    title:"Ok, tomese su tiempo " + nombrePersona
+                })
             }
             if(numRandom == 1){
-                console.log("Decida tranquilo y bien " + nombrePersona)
+                Swal.fire({
+                    position: 'top-end',
+                    timer: 2000,
+                    title:"Decida bien" + nombrePersona
+                })
             }
             if(numRandom == 2){
-                console.log("Nadie lo apura, tiene todo el tiempo que usted quiera. "+ nombrePersona+ " Decida bien")
+                Swal.fire({
+                    position: 'top-end',
+                    timer: 2000,
+                    title:"Nadie lo apura, tiene todo el tiempo que usted quiera. "+ nombrePersona+ " Decida bien"
+                })
             }        
         }
+    })
     }
     else{
-        alert("Usted no compro nada, seleccione algun producto.")
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Usted no selecciono ningun producto!',
+            footer: 'Seleccione algun producto en el inicio'
+          })
     }
 
 }
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let total = 0
 let productosAgregados = []
 
 let tabla = document.querySelector("#tabla")
+let barraBuscar = document.querySelector("#buscador")
+document.getElementById('total').innerHTML = total
 
 let month = parseInt(new Date().getMonth())
 let day = new Date().getUTCDate()
@@ -10,31 +12,55 @@ let year = new Date().getUTCFullYear()
 let buscar = document.getElementById("buscar")
 buscar.addEventListener("click", buscador)
 
-let barraBuscar = document.querySelector("#buscador")
-
 let cerrar = document.querySelector("#cerrar")
 cerrar.addEventListener("click", (a)=>{
     barraBuscar.value = ""
     buscador()
 })
 
-let nombrePersona = localStorage.getItem("Nombre")
-if( nombrePersona == null){
-    nombrePersona = prompt("Ingrese su nombre")
+const verificarNombre = () =>{
+var nombrePersona = localStorage.getItem("Nombre")
+
+if( nombrePersona === null||nombrePersona === "null"){
+    Swal.fire({
+        title: 'Como se llama?',
+        icon: 'question',
+        input:'text',
+    }).then((result) => {
+        nombrePersona = result.value
+        localStorage.setItem("Nombre", nombrePersona)
+    })
+
 }
 if (nombrePersona == ''){
-    console.warn("Algo me suena raro... Se llama null o acaso no tiene nombre?")
-    if (confirm("Quiere cambiar el nombre que puso o realmente se llama null?")){
-        nombrePersona = prompt("Como te llamas realmente?")
-    }
+    Swal.fire({
+        title: 'Quiere cambiar el nombre que figura en la web?',
+        text: "Su nombre por ahora no figura",
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ok!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Cual es su nombre?',
+                input: 'text',
+            }).then((result) => {
+                nombrePersona = result.value
+                localStorage.setItem("Nombre", nombrePersona)
+            })
+        }
+    })
+    localStorage.setItem("Nombre", nombrePersona)
 }
-
-document.getElementById('total').innerHTML = total
+}
+verificarNombre()
 
 let saludos = [
-    "Bienvenido a nuestro E-comerce " + nombrePersona,
+    "Bienvenido a nuestro E-comerce ",
     "Esperamos que disfrute su experiencia",
-    "Como esta? " + nombrePersona + " bienvenido!"
+    "Como esta? bienvenido!"
 ]
 const date = month+"-"+day+"-"+ year
 
@@ -44,58 +70,22 @@ let productos = [
     new articulo("Procesador I5 11 gen", 2000, 1, "prc" ,"assets/I5 11.png"),
     new articulo("Procesador I7 10 gen", 3500, 2, "prc" ,"assets/I7.png"),
     new articulo("Procesador Ryzen7 11 gen", 5000, 3, "prc" ,"assets/Ryzen7.png"),
-    new articulo("Procesador I9 10 gen", 10000, 4, "prc" ,"assets/I9.png"),
     new articulo("Procesador Ryzen9 11 gen", 25000, 5, "prc" ,"assets/Ryzen9.png"),
     new articulo("RTX 3060", 40000, 6, "pdv" ,"assets/RTX 3060.png"),
     new articulo("RTX 3090", 45000, 7, "pdv" ,"assets/RTX 3090.png"),
-    
+
 ]
+
 
 // _______________________________
 
 // Funciones
 
-function buscador(){ 
+function buscador(){
     let buscado = document.getElementById("buscador").value
     buscado = buscado.toUpperCase()
     const resultado = productos.filter((el) => el.nombre.toUpperCase().includes(buscado))
-    let fila = ""
-    tabla.innerHTML = ""
-    let tipo = ""
-    resultado.forEach(producto =>{
-        let { url, nombre, precio} = producto //Aca aplico lo que vimos en la clase
-        if (tipo === producto.tipo){
-            fila = `
-                    <div class="card" style="width: 180px;height: 365px;">
-                        <img src="${url}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">${nombre}</h5>
-                            <p class="card-text">Valor: ${precio}$</p>
-                            <input style=" position: absolute; top: 311px;" type="button" id="btn${producto.id}" value="Agregar al carrito"></input></div>
-                        </div>
-            `
-            
-            let productosMismoTipo = document.getElementById(producto.tipo)
-            productosMismoTipo.innerHTML += fila
-        }
-        else{
-            fila = `
-                    <br>
-                    <br>
-                    <div id="${producto.tipo}" style="display: flex;justify-content: space-evenly;">
-                        <div class="card" style="width: 180px;height: 365px;">
-                            <img src="${url}" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">${nombre}</h5>
-                                <p class="card-text">Valor: ${precio}$</p>
-                                <input style=" position: absolute; top: 311px;" type="button" id="btn${producto.id}" value="Agregar al carrito"></input></div>
-                        </div>
-                    </div>
-                `
-            tabla.innerHTML += fila
-        }
-        tipo = producto.tipo
-    })
+    cargarProductos(resultado)
 }
 
 function bienvenida(){
@@ -111,7 +101,7 @@ function cargarProductos(array){
     tabla.innerHTML = ""
     let tipo = ""
     array.forEach(producto =>{
-        let { url, nombre, precio} = producto //Aca aplico lo que vimos en la clase
+        let { url, nombre, precio} = producto
         if (tipo === producto.tipo){
             fila = `
                     <div class="card" style="width: 180px;height: 365px;">
@@ -149,7 +139,7 @@ function cargarProductos(array){
 cargarProductos(productos)
 
 productos.forEach(producto =>{
-    let{id}=producto //Aca aplico lo que vimos en la clase
+    let{id}=producto 
     let agregarAlCarrito = document.querySelector(`#btn${id}`) 
     agregarAlCarrito.addEventListener("click",a =>{
     productosAgregados.push(productos[id].nombre)
